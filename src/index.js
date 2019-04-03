56fa674a967ffb6be45dc4072ccca80d64871526
Luca Mele <luca.mele@axa-winterthur.ch>
2019-04-03 13:14:10 +0200

    Fix eslint issues

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss';
 import 'document-register-element';
 
-import '@axa-ch/patterns-library/lib/components/u-core/index.js';
-import '@axa-ch/patterns-library/lib/components/a-icon/index.js';
+import '@axa-ch/patterns-library/lib/components/u-core/index';
+import '@axa-ch/patterns-library/lib/components/a-icon/index';
+import { AXAButton } from '@axa-ch/patterns-library/lib/js/react-exports';
 
-import {
-  AXAButton,
-} from '@axa-ch/patterns-library/lib/js/react-exports';
+import './index.scss';
 
 export default class PodBaugarantie {
   // elem is the DOM node where the pod will be atatched on
   // options are the data attributes of the pod
   constructor(elem, options) {
-      this.elem = elem;
-      this.options = options;
-      this.init();
+    this.elem = elem;
+    this.options = options;
+    this.init();
   }
 
   init() {
     ReactDOM.render(<Fragment>
-      <axa-core only-load="true" icons-path="icons.svg"></axa-core>
+      <axa-core only-load="true" icons-path="icons.svg" />
       <AXAButton arrow motion>My Button medium</AXAButton>
     </Fragment>, this.elem);
   }
