3c44fb1524c10eb3eef16cc347d6d41f29a1a478
Luca Mele <lucamele1@hotmail.com>
2019-06-07 10:55:15 +0200

    After login redirect

--- src/components/app.js
+++ src/components/app.js
@@ -11,29 +11,35 @@ import Home from './pages/home';
 import Dashboard from './pages/dashboard';
 import Login from './pages/login/index';
 
-const App = ({ history, at }) => (
-  <AXAContainer>
-    {at ? (
-      <nav className="o-baug__app__nav">
-        <AXALink onClick={() => history.push('/')} >
-          Home
-        </AXALink>
-        <AXALink onClick={() => history.push('/dashboard')} >
-          Dashboard
-        </AXALink>
-      </nav>
-    ) : (
-      ''
-    )}
-    <article className="o-baug__app__content">
-      <Switch>
-        <PrivateRoute exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
-      </Switch>
-    </article>
-  </AXAContainer>
-);
+const App = ({ history, at }) => {
+  const { location: { pathname } } = history;
+  if (~pathname.indexOf('login') && at.length > 1) {
+    history.push('/dashboard');
+  }
+  return (
+    <AXAContainer>
+      {at ? (
+        <nav className="o-baug__app__nav">
+          <AXALink onClick={() => history.push('/')} >
+            Home
+          </AXALink>
+          <AXALink onClick={() => history.push('/dashboard')} >
+            Dashboard
+          </AXALink>
+        </nav>
+      ) : (
+        ''
+      )}
+      <article className="o-baug__app__content">
+        <Switch>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </article>
+    </AXAContainer>
+  )
+};
 
 App.propTypes = {
   history: PropTypes.object,
