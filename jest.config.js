5b2c0edb11c5dec48b502770ff01d541efa1422e
Luca Mele <lucamele1@hotmail.com>
2019-07-05 11:20:06 +0200

    Add test env

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,12 @@
 module.exports = {
-  collectCoverageFrom: [
-    'src/**/*.js',
-  ],
-  setupFilesAfterEnv: [
-    `${__dirname}/config/jest/setupTests.js`,
-  ],
-  testMatch: [
-    `${__dirname}/src/**/unit.test.{js,jsx,mjs}`,
-  ],
+  collectCoverageFrom: ['src/**/*.js'],
+  setupFilesAfterEnv: [`${__dirname}/config/jest/setupTests.js`],
+  testMatch: [`${__dirname}/src/**/unit.test.{js,jsx,mjs}`],
   transform: {
     '^.+\\.css$': `${__dirname}/config/jest/cssTransform.js`,
     '^(?!.*\\.(js|jsx|mjs|css|json)$)': `${__dirname}/config/jest/fileTransform.js`,
     '^.+\\.(js|jsx)$': `${__dirname}/config/jest/jestPreprocess.js`,
   },
-  transformIgnorePatterns: [
-    '/node_modules\\/(?![lit\\-element|lit\\-html])/',
-  ],
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-  ],
+  transformIgnorePatterns: ['/node_modules\\/(?![lit\\-element|lit\\-html])/'],
+  moduleFileExtensions: ['js', 'jsx', 'json'],
 };
