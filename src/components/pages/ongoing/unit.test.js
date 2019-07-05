40b1fc8f994d4b536e9d31b15678ce85ed8d8c7a
Luca Mele <lucamele1@hotmail.com>
2019-07-05 15:19:12 +0200

    Mocking the ongoing page

--- src/components/pages/ongoing/unit.test.js
+++ src/components/pages/ongoing/unit.test.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import Enzyme, { render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+import Ongoing from './index';
 
-import { AXAButton } from '../../patterns-library';
+Enzyme.configure({ adapter: new Adapter() });
 
-describe('AXA Button Test', () => {
-  it('contains 2 AXAButton', () => {
-    const wrapper = shallow(
-      <div>
-        <AXAButton />
-        <AXAButton />
-      </div>
-    );
-    expect(wrapper.find(AXAButton).length).toEqual(2);
+// create the store with the needed middlewears
+const mockStore = configureMockStore();
+
+function setup() {
+  const enzymeWrapper = render(
+    <Provider
+      store={mockStore({
+        lastSearch: '',
+        pageNumber: 0,
+        rowLength: 0,
+        needsPagination: false,
+        isSearching: false,
+        tableOriginalItems: {},
+        tableItems: {},
+      })}
+    >
+      <Router>
+        <Ongoing />
+      </Router>
+    </Provider>
+  ).dive();
+
+  return {
+    enzymeWrapper,
+  };
+}
+
+describe('Ongoing page', () => {
+  afterEach(() => {
+    // fetchMock.restore()
+  });
+  it('should render', () => {
+    const { enzymeWrapper } = setup();
+    console.log(enzymeWrapper);
+    console.log(enzymeWrapper.find('div').debug());
+    expect(enzymeWrapper.find('div').hasClass('lds-dual-ring')).toBe(true);
   });
 });
