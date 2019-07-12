99433a9e6551816276e34aae273dfc7da2b78ffb
Luca Mele <lucamele1@hotmail.com>
2019-07-12 15:23:37 +0200

    Add tests for search

--- src/components/pages/ongoing/unit.test.js
+++ src/components/pages/ongoing/unit.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { AXATableSortableReact } from '../../patterns-library';
+import { AXATableSortableReact, AXAButton, AXAInputTextReact } from '../../patterns-library';
 // import { HashRouter as Router } from 'react-router-dom';
 import { Ongoing } from './index';
+
+import { setSearch as originalSetSearch } from './actions';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -58,8 +60,16 @@ class Setup {
   }
 
   render() {
+    return this._render(mount);
+  }
+
+  shallow() {
+    return this._render(shallow);
+  }
+
+  _render(method) {
     const { setSearch, handlePagination, loadTableItems } = this;
-    return shallow(
+    return method(
       <Ongoing
         setSearch={setSearch}
         handlePagination={handlePagination}
@@ -76,10 +86,10 @@ describe('Ongoing page', () => {
     setup = new Setup();
   });
   it('should render loading animation', () => {
-    const enzymeWrapper = setup.render();
+    const enzymeWrapper = setup.shallow();
     expect(enzymeWrapper.hasClass('lds-dual-ring')).toBe(true);
   });
-  it('should render table', async () => {
+  it('should render correctly with data taken from an action', async () => {
     setup = new Setup({
       loadTableItems: () => {
         setup.store.tableItems = {
@@ -89,15 +99,123 @@ describe('Ongoing page', () => {
           ],
           [
             { html: '<span>1 Some Text</span>' },
-          ],]
+          ]]
         }
       }
     });
     // this will call loadTableItems
-    setup.render();
+    setup.shallow();
     // this will add the updated store
-    const enzymeWrapper = setup.render();
+    const enzymeWrapper = setup.shallow();
     expect(enzymeWrapper.hasClass('lds-dual-ring')).toBe(false);
     expect(enzymeWrapper.find(AXATableSortableReact).exists()).toBe(true);
+    expect(
+      enzymeWrapper.find('.o-baug__app__content-table-pagination')
+        .find(AXAButton)
+        .exists()
+    ).toBe(false);
+    expect(
+      enzymeWrapper.find('.o-baug__app__content-table-search')
+        .find(AXAInputTextReact)
+        .exists()
+    ).toBe(true);
+  });
+
+  it('should search and find a result', async () => {
+    const SEARCH_VALUE = 'some';
+    setup = new Setup({
+      setSearch: (text) => {
+        originalSetSearch(text)((action) => {
+          const { type, payload } = action;
+          if (type === 'BG_ONGOING_SEARCH_SET_VALUE') {
+            expect(payload.lastSearch).toBe(SEARCH_VALUE);
+            expect(payload.isSearching).toBe(true);
+          }
+          if (type === 'BG_ONGOING_SEARCH_SET_ITEMS') {
+            setup.store.tableItems = payload;
+            expect(payload.tbody.length).toBe(2);
+          }
+        }, () => ({
+          ongoing: setup.store
+        }));
+      }
+    });
+    const { store } = setup;
+    store.tableOriginalItems = {
+      thead: [{ html: 'Title', sort: 'ASC' }],
+      tbody: [[
+        { html: `<span>Here ${SEARCH_VALUE} content 1</span>` },
+      ],
+      [
+        { html: `<span>Here ${SEARCH_VALUE} content 2</span>` },
+      ]]
+    }
+    store.tableItems = store.tableOriginalItems;
+    const enzymeWrapper = setup.shallow();
+    enzymeWrapper.find('.o-baug__app__content-table-search')
+      .find(AXAInputTextReact)
+      .simulate('change', { target: { value: SEARCH_VALUE} });
+
+    await new Promise((resolve) => {
+      // action search as a timeout for debouncing. Here wait for the same amount of timeout
+      // plus a 100ms treshold
+      setTimeout(resolve, 400);
+    });
+    // re render with new data
+    setup.shallow();
+    expect(
+      enzymeWrapper
+        .find(AXATableSortableReact)
+        .props().model.tbody[0][0].html
+    ).toContain(SEARCH_VALUE);
+  });
+
+  it('should search and not find a result', async () => {
+    const SEARCH_VALUE = 'other';
+    setup = new Setup({
+      setSearch: (text) => {
+        originalSetSearch(text)((action) => {
+          const { type, payload } = action;
+          if (type === 'BG_ONGOING_SEARCH_SET_VALUE') {
+            expect(payload.lastSearch).toBe(SEARCH_VALUE);
+            expect(payload.isSearching).toBe(true);
+          }
+          if (type === 'BG_ONGOING_SEARCH_SET_ITEMS') {
+            setup.store.tableItems = payload;
+            expect(payload.tbody[0].length).toBe(0);
+          }
+        }, () => ({
+          ongoing: setup.store
+        }));
+      }
+    });
+    const { store } = setup;
+    store.tableOriginalItems = {
+      thead: [{ html: 'Title', sort: 'ASC' }],
+      tbody: [[
+        { html: `<span>Here some content 1</span>` },
+      ],
+      [
+        { html: `<span>Here some content 2</span>` },
+      ]]
+    }
+    store.tableItems = store.tableOriginalItems;
+    const enzymeWrapper = setup.shallow();
+    enzymeWrapper.find('.o-baug__app__content-table-search')
+      .find(AXAInputTextReact)
+      .simulate('change', { target: { value: SEARCH_VALUE} });
+
+    await new Promise((resolve) => {
+      // action search as a timeout for debouncing. Here wait for the same amount of timeout
+      // plus a 100ms treshold
+      setTimeout(resolve, 400);
+    });
+    // re render with new data
+    const enzymeWrapperAfterSearch = setup.shallow();
+    expect(
+      enzymeWrapperAfterSearch
+        .find(AXATableSortableReact)
+        .props().model.tbody[0].length
+    ).toBe(0);
   });
 });
