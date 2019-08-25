ccd920f1adcd7dc8707dd1c1732bfd1f0b89b282
Luca Mele <lucamele1@hotmail.com>
2019-08-25 13:41:11 +0200

    Add search UI Tests

--- src/components/pages/ongoing/ui.test.js
+++ src/components/pages/ongoing/ui.test.js
@@ -24,13 +24,112 @@ test('Should render table', async t => {
   await t.expect($el.exists).ok();
 });
 
+test('Should search table', async t => {
+  await t.navigateTo('/#/ongoing')
+  await t.typeText('input[name="search-table"]', '99' );
+  await t.wait( 600 );
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-table-sortable').shadowRoot
+  );
+  const $axaElemShadowEl = await $axaElemShadow.find('tbody tr');
+  await t.expect($axaElemShadowEl.count).eql(1);
+});
+
+test('Should render big table', async t => {
+  await t.navigateTo('/#/ongoing');
+
+  const dropdown = await Selector(() =>
+    document.querySelector('axa-dropdown')
+  );
+  const secondOption = await Selector(() =>
+    document.querySelector('axa-dropdown button[data-index="1"]')
+  );
+  await t.click(dropdown);
+  await t.click(secondOption);
+
+  await t.wait( 1400 );
+
+  const $elPagination = await Selector('.o-baug__app__content-table-pagination-text').addCustomDOMProperties({
+    innerHTML: el => el.innerHTML,
+  });
+
+  await t.expect($elPagination.innerHTML).eql('Seite 1 von 201');
+
+  await t.typeText('input[name="search-table"]', '999' );
+
+  await t.wait( 600 );
+
+  const $elPaginationFilter = await Selector('.o-baug__app__content-table-pagination-text').addCustomDOMProperties({
+    innerHTML: el => el.innerHTML,
+  });
+
+  await t.expect($elPaginationFilter.innerHTML).eql('Seite 1 von 3');
+
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-table-sortable').shadowRoot
+  );
+  const $axaElemShadowEl = await $axaElemShadow.find('tbody tr');
+  await t.expect($axaElemShadowEl.count).eql(100);
+});
+
+
+// HERE ALL MOBILE TESTS. DUPLICATION OF CODE HERE IS INTENDED
+
 
 fixture('PAGE Ongoing - basic functionality mobile')
   .page(`${host}/`)
   .beforeEach(beforeEachHook(true));
 
-test('Should render table', async t => {
+test('Should render table mobile', async t => {
   await t.navigateTo('/#/ongoing')
   const $el = await Selector('axa-table-sortable');
   await t.expect($el.exists).ok();
+});
+
+test('Should search table mobile', async t => {
+  await t.navigateTo('/#/ongoing')
+  await t.typeText('input[name="search-table"]', '99' );
+  await t.wait( 600 );
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-table-sortable').shadowRoot
+  );
+  const $axaElemShadowEl = await $axaElemShadow.find('tbody tr');
+  await t.expect($axaElemShadowEl.count).eql(1);
+});
+
+test('Should render big table on mobile', async t => {
+  await t.navigateTo('/#/ongoing');
+
+  const dropdown = await Selector(() =>
+    document.querySelector('axa-dropdown')
+  );
+  const secondOption = await Selector(() =>
+    document.querySelector('axa-dropdown button[data-index="1"]')
+  );
+  await t.click(dropdown);
+  await t.click(secondOption);
+
+  await t.wait( 1400 );
+
+  const $elPagination = await Selector('.o-baug__app__content-table-pagination-text').addCustomDOMProperties({
+    innerHTML: el => el.innerHTML,
+  });
+
+  await t.expect($elPagination.innerHTML).eql('Seite 1 von 201');
+
+  await t.typeText('input[name="search-table"]', '999' );
+
+  await t.wait( 600 );
+
+  const $elPaginationFilter = await Selector('.o-baug__app__content-table-pagination-text').addCustomDOMProperties({
+    innerHTML: el => el.innerHTML,
+  });
+
+  await t.expect($elPaginationFilter.innerHTML).eql('Seite 1 von 3');
+
+  const $axaElemShadow = await Selector(
+    () => document.querySelector('axa-table-sortable').shadowRoot
+  );
+  const $axaElemShadowEl = await $axaElemShadow.find('tbody tr');
+  await t.expect($axaElemShadowEl.count).eql(100);
 });
