04ed0a221208a614ca66c8c1efec3e143a1663e0
Luca Mele <lucamele1@hotmail.com>
2019-12-16 13:31:05 +0100

    Fix tests

--- src/components/pages/new/ui.test.js
+++ src/components/pages/new/ui.test.js
@@ -2,48 +2,45 @@ import { Selector, ClientFunction } from 'testcafe';
 
 const host = 'http://localhost:9999';
 
-const beforeEachHook = (mobile = true) => async (t) => {
+const beforeEachHook = (mobile = true) => async t => {
   if (mobile) {
     await t.resizeWindow(350, 600);
   } else {
     await t.maximizeWindow();
   }
-  await t.typeText( 'input[name="username"]', 'testuser' );
-  await t.typeText( 'input[name="password"]', 'testpassword' );
+  await t.typeText('input[name="username"]', 'testuser');
+  await t.typeText('input[name="password"]', 'testpassword');
   await t.click('axa-button[type="submit"]');
-  await t.wait( 300 );
-}
+  await t.wait(300);
+};
 
 fixture('PAGE New - basic functionality desktop')
   .page(`${host}/`)
   .beforeEach(beforeEachHook(false));
 
 test('Should render form desktop', async t => {
-  await t.navigateTo('/#/new')
+  await t.navigateTo('/#/new');
   const $el = await Selector('.o-baug__app__contract-form-wrapper');
   await t.expect($el.exists).ok();
 });
 
 test('Should fill form desktop', async t => {
-  await t.navigateTo('/#/new')
-  await t.typeText( 'input[name="company_name"]', 'test_data' );
-  await t.typeText( 'input[name="company_name_1"]', 'test_data 1' );
-  await t.typeText( 'input[name="company_name_2"]', 'test_data 2' );
-  await t.typeText( 'input[name="place"]', 'Winterthur' );
-  await t.typeText( 'input[name="post_code"]', '8406' );
-
+  await t.navigateTo('/#/new');
+  await t.typeText('input[name="company_name"]', 'test_data');
+  await t.typeText('input[name="company_name_1"]', 'test_data 1');
+  await t.typeText('input[name="company_name_2"]', 'test_data 2');
+  await t.typeText('input[name="place"]', 'Winterthur');
+  await t.typeText('input[name="post_code"]', '8406');
 
   const openCalendar = await Selector(() =>
     document
       .querySelector(`axa-datepicker`)
       .querySelector('button[class*="m-datepicker__input-button"]')
-  )
+  );
   await t.click(openCalendar, { speed: 0.5 });
 
   const dropDown = await Selector(() =>
-    document
-      .querySelector('axa-datepicker')
-      .querySelector('axa-dropdown')
+    document.querySelector('axa-datepicker').querySelector('axa-dropdown')
   );
 
   await t.click(dropDown, { speed: 0.5 });
@@ -52,7 +49,7 @@ test('Should fill form desktop', async t => {
       .querySelector(`axa-datepicker`)
       .querySelector('axa-dropdown[class*="js-datepicker__dropdown-month"]')
       .querySelector('button[data-index="1"]')
-  )
+  );
   await t.click(monthFebruary, { speed: 0.5 });
 
   const dayToSelect = await Selector(() => {
@@ -78,18 +75,17 @@ test('Should fill form desktop', async t => {
   await t.click(submitButton, { speed: 0.5 });
 
   const getInputValue = ClientFunction(
-    () =>
-      document.querySelector(
-        `axa-datepicker`
-      ).value
+    () => document.querySelector(`axa-datepicker`).value
   );
-  await t.expect(await getInputValue()).eql('15.2.2019');
+  await t.expect(await getInputValue()).eql('15.1.2019');
 
   const dropdown = await Selector(() =>
     document.querySelector('axa-dropdown[title="Schweiz"]')
   );
   const secondOption = await Selector(() =>
-    document.querySelector('axa-dropdown[title="Schweiz"] button[data-index="1"]')
+    document.querySelector(
+      'axa-dropdown[title="Schweiz"] button[data-index="1"]'
+    )
   );
   await t.click(dropdown, { speed: 0.5 });
   await t.click(secondOption, { speed: 0.5 });
@@ -100,9 +96,11 @@ test('Should fill form desktop', async t => {
     return Object.fromEntries(new FormData(form).entries());
   });
 
-  await t.expect(JSON.stringify(await formData())).eql(
-    '{"company_name":"test_data","company_name_1":"test_data 1","company_name_2":"test_data 2","post_code":"8406","place":"Winterthur","countries":"li"}'
-  );
+  await t
+    .expect(JSON.stringify(await formData()))
+    .eql(
+      '{"company_name":"test_data","company_name_1":"test_data 1","company_name_2":"test_data 2","post_code":"8406","place":"Winterthur","countries":"li"}'
+    );
 });
 
 // HERE ALL MOBILE TESTS. DUPLICATION OF CODE HERE IS INTENDED
@@ -112,7 +110,7 @@ fixture('PAGE New - basic functionality mobile')
   .beforeEach(beforeEachHook(true));
 
 test('Should render form mobile', async t => {
-  await t.navigateTo('/#/new')
+  await t.navigateTo('/#/new');
   const $el = await Selector('.o-baug__app__contract-form-wrapper');
   await t.expect($el.exists).ok();
 });
